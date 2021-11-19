0576b17990f1b026223d9363133743457c7e4625
Jedapaji <jessicadpaez@outlook.com>
2021-11-18 20:41:50 -0500

    Primera pagina de test

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -14,13 +14,106 @@ function Page1() {
                   justifyContent:'center',
                 }}>
             <ol>
-                <p>1. ¿Aceptarías trabajar escribiendo artículos en la sección económica de un diario?</p>
+                <li>¿Aceptarías trabajar escribiendo artículos en la sección económica de un diario?</li>
+                <p/>
                 <Button variant="contained">Si</Button>&nbsp;
                 <Button variant="contained">No</Button>
                 <p/>
-                <p>2. ¿Te ofrecerías para organizar la despedida de soltero de uno de tus amigos?</p>
+                <li>¿Te ofrecerías para organizar la despedida de soltero de uno de tus amigos?</li>
+                <p/>
                 <Button variant="contained">Si</Button>&nbsp;
                 <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Te gustaría dirigir un proyecto de urbanización en tu ciudad o municipio?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Ante una frustración siempre contrapones un pensamiento positivo?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Te dedicarías a socorrer a personas accidentadas o atacadas por asaltantes?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Cuando eras pequeñ@, te interesaba saber cómo estaban construidos tus juguetes?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Te interesan más los misterios de la naturaleza que los secretos de la tecnología?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Escuchas atentamente los problemas que te comentan tus amigos?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Eres exigente y crítico con tu equipo de trabajo?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Te atrae armar rompecabezas o puzzles?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Puedes establecer la diferencia conceptual entre macroeconomía y microeconomía?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Usar uniforme te hace sentir distinto, importante?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Participarías como profesional en un espectáculo de acrobacia aérea?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Organizas tu dinero de manera que te alcance hasta el próximo pago?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Convences fácilmente a otras personas sobre la validez de tus argumentos?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Estás informado sobre los nuevos descubrimientos que se están realizando sobre la Teoría del Big-Bang?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Ante una situación de emergencia actuas rápidamente?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Cuando tienes que resolver un problema matemático, perseveras hasta encontrar la solución?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Si te convocara tu club preferido para planificar, organizar y dirigir un campo de deportes, aceptarías?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+                <p/>
+                <li>¿Eres el que pone un toque de alegría en las fiestas?</li>
+                <p/>
+                <Button variant="contained">Si</Button>&nbsp;
+                <Button variant="contained">No</Button>
+
                 <p/>
             </ol>
             </Box>
