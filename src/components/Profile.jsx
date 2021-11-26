62ec12a62449fb2ddc4ad1e3a6bab3ff23e25765
polware <polware@gmail.com>
2021-11-25 22:32:25 -0500

    Se agrega la página del perfil del estudiante.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,32 +1,66 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import React, { useState, useEffect} from 'react'
+import Axios from 'axios'
 import { useHistory } from "react-router-dom";
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 export default function Profile() {
+    const [identificacion, setIdentificacion] = useState('')
     const history = useHistory();
     const navigateTo1 = () => history.push('/Actualizar')
     const navigateTo2 = () => history.push('/Eliminar')
     const navigateTo3 = () => history.push('/Test')
     const navigateTo4 = () => history.push('/Resultados')
 
+    useEffect( ()=>{
+        obtenerEstudiante()
+    },[] )
+
+    const obtenerEstudiante = async()=>{        
+        const id = sessionStorage.getItem('idusuario')
+        const token = sessionStorage.getItem('token')
+        const respuesta = await Axios.get('/estudiante/listarID/'+id,{
+          headers:{'autorizacion':token}
+        })  
+        console.log(respuesta.data)
+        setIdentificacion("ID Estudiante: "+respuesta.data.identificacion)
+      }
+
     return (
-        <Box
-        sx={{
-          marginTop: 6,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-      <Typography component="h1" variant="h3" color="blue">Perfil Estudiante</Typography>
-      <br/>
-      <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2, ml: 3 }} color="info" onClick={navigateTo1}>Actualizar Datos</Button>&nbsp;
-      <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2, ml: 3 }} color="info" onClick={navigateTo2}>Eliminar Perfil</Button>&nbsp;
-      <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2, ml: 3 }} color="info" onClick={navigateTo3}>Test CHASIDE</Button>&nbsp;
-      <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2, ml: 3 }} color="info" onClick={navigateTo4}>Resultados Test</Button>&nbsp;
-      </Box>
+        <div>
+            <br/>            
+            <h2>Perfil del Estudiante</h2>
+            <hr/>
+            <Box sx={{ marginTop: 5, display: 'flex', flexDirection: 'column', alignItems: 'center', }} >
+            <div>
+                <Stack direction="row" justifyContent="center" alignItems="center" spacing={7}>
+                    <img src="/img/Profile.jpg" alt="perfil" width="260" height="260"/>
+                    <img src="/img/Update.jpg" alt="actualizar" width="250" height="250"/>
+                    <img src="/img/Delete.png" alt="eliminar" width="190" height="190"/>
+                    <img src="/img/Test.png" alt="test" width="200" height="200"/>
+                    <img src="/img/Results.png" alt="resultado" width="200" height="200"/>
+                </Stack>
+            </div>
+            </Box>
+            <Box sx={{ marginTop: 3, display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
+            <div>
+                <Stack direction="row" marginRight='22px' justifyContent="center" alignItems="center" spacing={13}>
+                    <TextField disabled id="filled-basic" label="" variant="filled" value={identificacion} />
+                    <Button type="submit" variant="contained" sx={{ mt: 1, mb: 1, right: 12 }} color="info" onClick={navigateTo1}>Actualizar Datos</Button>
+                    <Button type="submit" variant="contained" sx={{ mt: 1, mb: 1 }} color="info" onClick={navigateTo2}>Eliminar Perfil</Button>
+                    <Button type="submit" variant="contained" sx={{ mt: 1, mb: 1 }} color="info" onClick={navigateTo3}>Test CHASIDE</Button>
+                    <Button type="submit" variant="contained" sx={{ mt: 1, mb: 1 }} color="info" onClick={navigateTo4}>Resultados</Button>
+                </Stack>                
+            </div>
+            </Box>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+        </div>
     )
 }
-
